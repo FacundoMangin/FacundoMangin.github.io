6ff9a775a33e85bcbb56049d525a79f765f17372
FacundoMangin <125519551+FacundoMangin@users.noreply.github.com>
2023-03-27 19:38:52 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="shortcut icon" href="imagenes/icono.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="css/Main.css">
+
+
+
+    <script src="https://kit.fontawesome.com/092fed5d7c.js" crossorigin="anonymous"></script>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
+    <!-- pagina de google fonts  https://fonts.google.com/specimen/Lobster/tester-->
+    
+    <!-- letra Concert -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
+     <!-- letra Rubik -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <!-- letra Style  font-family: 'Crete Round'  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crete+Round&display=swap" rel="stylesheet">
+    
+    
+    <title>Commander 3D</title>
+</head>
+
+<body>
+    <header>
+        <?php include('./header-navegacion.html')?>
+    </header>
+
+
+    <main>
+        <div class="resina-filamento">
+            <?php include('./diferencia-resina.html')?>
+        </div>
+        
+        <div class="carrousel">
+            <?php include('./carrusel.html')?>
+        </div>
+    </main>
+
+    <footer class="footer">
+    <?php include('./footer.html')?>
+    </footer>
+    
+    
+    <div class="derechos-de-autor">
+        <p>© Copyright 2023 por Facundo Mangin</p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
