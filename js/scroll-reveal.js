b36eebdfa5ef37d0f0e6797fc19fd69d1177b449
Facundo Mangin <facundo.mangin@gmail.com>
2023-04-25 23:27:46 -0300

    Movimiento de la pagina

---
+++ js/scroll-reveal.js
@@ -0,0 +1,92 @@
+//nav
+window.sr = ScrollReveal();
+  sr.reveal('nav .container', {
+    duration: 2500,
+    distance: '30px',
+    origin: 'top'
+  });
+
+// header
+
+window.sr = ScrollReveal();
+  sr.reveal('.textos-header', {
+    duration: 2500,
+    distance: '400px',
+    origin: 'top',
+    
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.logo-presentacion', {
+    duration: 3000,
+  });
+
+
+
+// Diderencia entre recina y filamento
+
+  window.sr = ScrollReveal();
+  sr.reveal('.titulo-diferencia', {
+    duration: 1500,
+    distance: '400px',
+    origin: 'top'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.contenidos-textos', {
+    duration: 1500,
+    distance: '400px',
+    origin: 'ring'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.imagen-resina', {
+    duration: 1500,
+    distance: '400px',
+    origin: 'ring'
+  });
+  
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.imagen-about-us', {
+    duration: 4000,
+  });
+
+
+
+//Carrusel
+
+  window.sr = ScrollReveal();
+  sr.reveal('#carrusel', {
+    duration: 1200,
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.boton', {
+    duration: 2000,
+    rotate: {
+        x: 0,
+        y: 180,
+    }
+  });
+
+//footer
+
+window.sr = ScrollReveal();
+  sr.reveal('#footer', {
+    duration: 1200,
+  });
+
+window.sr = ScrollReveal();
+  sr.reveal('#tarjeta-facundo', {
+    duration: 2000,
+    distance: '400px',
+    origin: 'left'
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('#tarjeta-ivo', {
+    duration: 2000,
+    distance: '400px',
+    origin: 'ring'
+  });
