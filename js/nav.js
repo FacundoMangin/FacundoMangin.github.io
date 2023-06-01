9fb3dda1c6646490f2c7c8c2fa73892bdfc9c2b0
Facundo Mangin <facundo.mangin@gmail.com>
2023-05-31 22:21:50 -0300

    Arreglo y acomodo de material

--- js/nav.js
+++ js/nav.js
@@ -16,7 +16,7 @@ window.addEventListener("scroll", function() {
         
     } else {
         /* --sino se oculta añadiendo un top negativo */
-        $nav.style.top = "-220px";
+        $nav.style.top = "-350px";
     }
 
     /* --actulizo la ubicacion principal */
